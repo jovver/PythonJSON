4d72695b1b38ca910929c3dc4f4fd6fdd243ad19
Jore Vergara <JonV@magenic.com>
2021-04-27 00:51:22 +0800

    Updated file names and added .gitignore file

--- SendMessage_TestData.py
+++ SendMessage_TestData.py
@@ -6,13 +6,13 @@ numOfObjects = 500
 
 
 # Opens the template file
-with open("SendEmailRequest.json", "r") as file:
+with open("SendMessageRequest.json", "r") as file:
     data = json.load(file)
     file.close()
 
 
-# Acquires the emailData object to copy
-emailDataList = data["emailData"].copy()
+# Acquires the messageData object to copy
+messageDataList = data["messageData"].copy()
 copiedEmailData = copy.deepcopy(emailDataList[0])
 
 # Iterates over the number initialized
@@ -20,13 +20,13 @@ for i in range(numOfObjects):
     copiedSendData = copy.deepcopy(copiedEmailData["sendData"])
     copiedSendData.update({"accountPendingEventId": "123-" + str(i+1)})
     copiedEmailData.update({"sendData": copiedSendData})
-    emailDataList.append(copy.deepcopy(copiedEmailData))
+    messageDataList.append(copy.deepcopy(copiedEmailData))
 
 # Updates the dictionary object with the new list
-data.update({"emailData": emailDataList})
+data.update({"messageData": messageDataList})
 
 
 # Writes the result to a different json file
-with open("SendEmailRequest_Final.json", "w") as file:
+with open("SendMessageRequest_Final.json", "w") as file:
     json.dump(data, file)
     file.close()
